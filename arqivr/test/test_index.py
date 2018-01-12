207319df825a5619b7d70c6e53f6759e0256a321
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-01-12 10:47:43 +0000

    Add unit tests for ObjectIndex class.

--- arqivr/test/test_index.py
+++ arqivr/test/test_index.py
@@ -8,13 +8,14 @@ import shutil
 import time
 from arqivr.index import FilesystemObjectType
 from arqivr.index import FilesystemObject
+from arqivr.index import ObjectIndex
 
 #
 # Tests
 class TestFilesystemObject(unittest.TestCase):
     def setUp(self):
         # Create a temp working dir
-        self.wd = tempfile.mkdtemp(suffix='TestFilesystemObject') 
+        self.wd = tempfile.mkdtemp(suffix='TestFilesystemObject')
         self.pwd = os.getcwd()
         os.chdir(self.wd)
 
@@ -198,3 +199,69 @@ class TestFilesystemObject(unittest.TestCase):
         self.assertTrue(FilesystemObject("test.lnk").isaccessible)
         os.chmod("test.dir",0055)
         self.assertFalse(FilesystemObject("test.dir").isaccessible)
+
+class TestObjectIndex(unittest.TestCase):
+    def setUp(self):
+        # Create a temp working dir
+        self.wd = tempfile.mkdtemp(suffix='TestObjectIndex')
+        self.pwd = os.getcwd()
+        os.chdir(self.wd)
+
+    def tearDown(self):
+        os.chdir(self.pwd)
+        for root,dirs,files in os.walk(self.wd):
+            for name in dirs:
+                os.chmod(os.path.join(root,name),0755)
+            for name in files:
+                try:
+                    os.chmod(os.path.join(root,name),0644)
+                except OSError:
+                    pass
+        shutil.rmtree(self.wd)
+
+    def test_empty_objectindex(self):
+        indx = ObjectIndex(self.wd)
+        self.assertEqual(indx.names,[])
+        self.assertEqual(len(indx),0)
+
+    def test_populated_objectindex(self):
+        # Add some objects to current dir
+        dirs = ("test1.dir",
+                "test2.dir",
+                "test1.dir/sub.dir",)
+        files = ("test.txt",
+                 "test1.dir/test.txt",
+                 "test2.dir/test.txt",)
+        symlinks = { "test.lnk": "test.txt",
+                     "test1.dir/sub.dir/test.lnk": "../test.txt" }
+        for d in dirs:
+            os.mkdir(d)
+        for f in files:
+            with open(f,"w") as fp:
+                fp.write("test\n")
+        for s in symlinks:
+            os.symlink(symlinks[s],s)
+        # Build the index
+        indx = ObjectIndex(self.wd)
+        indx.build()
+        self.assertEqual(len(indx),8)
+        # Check __contains__
+        for d in dirs:
+            self.assertTrue(d in indx)
+        for d in files:
+            self.assertTrue(f in indx)
+        for s in symlinks:
+            self.assertTrue(s in indx)
+        self.assertFalse("missing" in indx)
+        # Check names
+        for n in indx.names:
+            self.assertTrue((n in dirs) or
+                            (n in files) or
+                            (n in symlinks))
+        # Check __getitem__
+        for n in indx.names:
+            obj = indx[n]
+            self.assertTrue(isinstance(obj,
+                                       FilesystemObject))
+            self.assertEqual(obj.path,
+                             os.path.join(self.wd,n))
